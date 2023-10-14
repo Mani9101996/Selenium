aabb0d6b4329de6e40cce1eae70f8340cdfd8ffa
Mani9101996 <143876493+Mani9101996@users.noreply.github.com>
2023-10-14 15:31:02 +0530

    Maven project

---
+++ com.TestMavenProject/src/test/java/com/TestMavenProject/TestClass1.java
@@ -0,0 +1,19 @@
+package com.TestMavenProject;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+@Test
+	public void testclass1() {
+		System.out.println("this is test case 1");
+	}
+	@Test
+	public void testclass2() {
+		System.out.println("this is test case 2");
+	}
+	@Test
+	public void testclass3() {
+		System.out.println("this is test case 3");
+	}
+	
+}
---
+++ com.TestMavenProject/src/test/java/com/TestMavenProject/TestClass2.java
@@ -0,0 +1,17 @@
+package com.TestMavenProject;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	@Test
+	public void testclass4() {
+		System.out.println("this is test case 4");
+	}
+	@Test
+	public void testclass5() {
+		System.out.println("this is test case 5");
+	}@Test
+	public void testclass6() {
+		System.out.println("this is test case 6");
+	}
+}
